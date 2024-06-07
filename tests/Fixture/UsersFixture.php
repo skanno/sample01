94f6b515e119ef6f3dd72c5cb28703bd566dc994
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2024-06-07 07:24:35 +0000

    feat: 各種ファイルをスキャフォールド

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'created' => '2024-06-03 08:25:40',
+                'modified' => '2024-06-03 08:25:40',
+            ],
+        ];
+        parent::init();
+    }
+}
